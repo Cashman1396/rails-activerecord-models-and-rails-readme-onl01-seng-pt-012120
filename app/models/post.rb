127f926f0708b40c276f0dc9c1e7fe9a8d241b09
Chris Ashman <chrisashman96@gmail.com>
2020-05-19 14:09:00 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   attr_accessor :post
 
     def post_summary
-        @post.title + " - " + @post.description
+        post.title + " - " + post.description
     end
 end
